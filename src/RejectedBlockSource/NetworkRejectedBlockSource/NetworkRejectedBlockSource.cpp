638a0fbb7eaac76e91702e6fb166123877949fb3
user <sv_91@inbox.ru>
2019-12-20 11:03:30 +0300

    Начал добавлять NetworkRejectedBlockSource

---
+++ src/RejectedBlockSource/NetworkRejectedBlockSource/NetworkRejectedBlockSource.cpp
@@ -0,0 +1,100 @@
+#include "NetworkRejectedBlockSource.h"
+
+#include "check.h"
+#include "log.h"
+
+#include "BlockchainRead.h"
+
+using namespace common;
+
+namespace torrent_node_lib {
+
+static const size_t MAXIMUM_REJECTED_BLOCKS = 1000;
+
+std::pair<std::vector<std::vector<unsigned char>>, std::vector<size_t>> NetworkRejectedBlockSource::getMissingBlocks(const std::vector<RejectedBlockMessage> &headers) const {
+    std::vector<std::vector<unsigned char>> missingHashes;
+    std::vector<size_t> missingIndices;
+
+    const auto &iterHashFabric = blocks.get<BlockHolder::HashTag>();
+    for (size_t i = 0; i < headers.size(); i++) {
+        const RejectedBlockMessage &block = headers[i];
+        const auto &hash = block.hash;
+        const auto found = iterHashFabric.find(hash);
+        if (found != iterHashFabric.end()) {
+            missingHashes.emplace_back(hash);
+            missingIndices.emplace_back(i);
+        }
+    }
+
+    return std::make_pair(missingHashes, missingIndices);
+}
+
+std::vector<NetworkRejectedBlockSource::BlockHolder> NetworkRejectedBlockSource::parseBlocks(const std::vector<RejectedBlockMessage> &headers, const std::vector<std::string> &blockDumps, const std::vector<size_t> &missingIndices) const {
+    CHECK(blockDumps.size() == missingIndices.size(), "Incorrect block dumps");
+
+    std::vector<BlockHolder> holders;
+    for (size_t i = 0; i < missingIndices.size(); i++) {
+        const RejectedBlockMessage &blockHeader = headers.at(missingIndices[i]);
+        const std::string &dump = blockDumps[i];
+        if (dump.empty()) {
+            continue;
+        }
+
+        BlockHolder holder;
+        holder.dump = dump;
+        holder.number = blockHeader.blockNumber;
+
+        holder.info = parseRejectedTxsBlockInfo(dump.data(), dump.data() + dump.size(), 0, true);
+
+        CHECK(holder.info.header.hash == blockHeader.hash && holder.info.header.timestamp == blockHeader.timestamp, "Incorrect parsed block");
+
+        holders.emplace_back(holder);
+    }
+
+    return holders;
+}
+
+void NetworkRejectedBlockSource::addNewBlocks(const std::vector<BlockHolder> &holders) {
+    for (const BlockHolder &holder: holders) {
+        blocks.insert(holder);
+    }
+    auto &iterSequenceFabric = blocks.get<BlockHolder::SequenceTag>();
+    if (iterSequenceFabric.size() > MAXIMUM_REJECTED_BLOCKS) {
+        const size_t countRemoved = iterSequenceFabric.size() - MAXIMUM_REJECTED_BLOCKS;
+        iterSequenceFabric.erase(iterSequenceFabric.begin(), std::next(iterSequenceFabric.begin(), countRemoved));
+    }
+}
+
+std::vector<RejectedBlockResult> NetworkRejectedBlockSource::getLastBlocks(size_t count) const {
+    std::vector<RejectedBlockResult> results;
+
+    const auto &iterSequenceFabric2 = blocks.get<BlockHolder::SequenceTag>();
+
+    std::transform(iterSequenceFabric2.rbegin(), std::next(iterSequenceFabric2.rbegin(), std::min(count, iterSequenceFabric2.size())), std::back_inserter(results), [](const BlockHolder &holder) {
+        return RejectedBlockResult(holder.hash(), holder.number, holder.timestamp());
+    });
+
+    return results;
+}
+
+std::vector<RejectedBlockResult> NetworkRejectedBlockSource::calcLastBlocks(size_t count) {
+    const std::vector<RejectedBlockMessage> blockHeaders = getterBlocks.getLastRejectedBlocks(count);
+
+    std::unique_lock<std::mutex> lock(mut);
+    const auto [missingHashes, missingIndices] = getMissingBlocks(blockHeaders);
+    CHECK(missingHashes.size() == missingIndices.size(), "Incorrect missing blocks");
+    lock.unlock();
+
+    const std::vector<std::string> blockDumps = getterBlocks.getRejectedBlocksDumps(missingHashes);
+
+    const std::vector<BlockHolder> holders = parseBlocks(blockHeaders, blockDumps, missingIndices);
+
+    lock.lock();
+    addNewBlocks(holders);
+    lock.unlock();
+
+    lock.lock();
+    return getLastBlocks(count);
+}
+
+} // namespace torrent_node_lib {
---
+++ src/RejectedBlockSource/NetworkRejectedBlockSource/NetworkRejectedBlockSource.h
@@ -0,0 +1,95 @@
+#ifndef TORRENT_NODE_NETWORKREJECTEDBLOCKSOURCE_H
+#define TORRENT_NODE_NETWORKREJECTEDBLOCKSOURCE_H
+
+#include <vector>
+#include <map>
+#include <string>
+#include <optional>
+#include <deque>
+#include <mutex>
+
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/random_access_index.hpp>
+#include <boost/multi_index/hashed_index.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/mem_fun.hpp>
+#include <boost/multi_index/composite_key.hpp>
+
+#include "blockchain_structs/RejectedTxsBlock.h"
+
+#include "RejectedBlockSource/RejectedBlockSource.h"
+
+#include "GetNewRejectedBlocksFromServer.h"
+
+namespace torrent_node_lib {
+
+class NetworkRejectedBlockSource: public RejectedBlockSource {
+private:
+
+    struct BlockHolder {
+        std::string dump;
+        RejectedTxsBlockInfo info;
+        size_t number;
+
+        std::vector<unsigned char> hash() const {
+            return info.header.hash;
+        }
+
+        size_t timestamp() const {
+            return info.header.timestamp;
+        }
+
+        struct SequenceTag {};
+
+        struct HashTag {};
+    };
+
+    using BlocksContainer = boost::multi_index::multi_index_container<
+        BlockHolder,
+        boost::multi_index::indexed_by<
+            boost::multi_index::ordered_non_unique<
+                boost::multi_index::tag<BlockHolder::SequenceTag>,
+                boost::multi_index::composite_key<
+                    BlockHolder,
+                    boost::multi_index::member<BlockHolder, size_t, &BlockHolder::number>,
+                    boost::multi_index::const_mem_fun<BlockHolder, size_t, &BlockHolder::timestamp>
+                >
+            >,
+            boost::multi_index::hashed_non_unique<
+                boost::multi_index::tag<BlockHolder::HashTag>,
+                boost::multi_index::const_mem_fun<BlockHolder, std::vector<unsigned char>, &BlockHolder::hash>
+            >
+        >
+    >;
+
+public:
+
+    std::vector<RejectedBlockResult> calcLastBlocks(size_t count) override;
+
+    std::vector<RejectedBlock> getBlocks(const std::vector<std::vector<unsigned char>> &hashes) const override;
+
+    std::vector<std::string> getDumps(const std::vector<std::vector<unsigned char>> &hashes) const override;
+
+private:
+
+    std::pair<std::vector<std::vector<unsigned char>>, std::vector<size_t>> getMissingBlocks(const std::vector<RejectedBlockMessage> &headers) const;
+
+    std::vector<BlockHolder> parseBlocks(const std::vector<RejectedBlockMessage> &headers, const std::vector<std::string> &blockDumps, const std::vector<size_t> &missingIndices) const;
+
+    void addNewBlocks(const std::vector<BlockHolder> &holders);
+
+    std::vector<RejectedBlockResult> getLastBlocks(size_t count) const;
+
+private:
+
+    GetNewRejectedBlocksFromServer getterBlocks;
+
+    BlocksContainer blocks;
+
+    mutable std::mutex mut;
+};
+
+} // namespace torrent_node_lib {
+
+#endif //TORRENT_NODE_NETWORKREJECTEDBLOCKSOURCE_H
